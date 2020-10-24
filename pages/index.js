a46f48863476ba838a6be19359b53566dd60d406
AlexCsanyi <alex.csanyi@gmail.com>
2020-10-24 16:17:35 +0100

    RBD-11 - adjust landing page style to more dummy product data

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,19 @@ import { Box, Flex } from "reflexbox";
 import Card from "../components/Card";
 
 export default function Home({ products }) {
-  console.log(products);
-
   return (
     <Box variant="container">
       <Box as="h2" my={40}>
         Featured Products
       </Box>
-      <Flex justifyContent="space-between" flexDirection={{ _: "column", md: "row" }} mb={100}>
+      <Flex
+        justifyContent="space-between"
+        flexDirection={{ _: "column", md: "row" }}
+        mb={100}
+        flexWrap="wrap"
+      >
         {products.map((product) => (
-          <Box key={product.id} width={{ _: "100%", md: "30%" }}>
+          <Box my={20} key={product.id} width={{ _: "100%", md: "30%" }}>
             <Card product={product}></Card>
           </Box>
         ))}
