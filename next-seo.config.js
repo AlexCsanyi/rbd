ec802113f56868b718bc177f70837cf2914cc01b
AlexCsanyi <alex.csanyi@gmail.com>
2020-10-25 14:56:20 +0000

    RBD-14 - add seo for later optimization + test

---
+++ next-seo.config.js
@@ -0,0 +1,13 @@
+export default {
+  title: "RB Dream",
+  description: "Fishing and pet shop in Kikinda, Serbia",
+  openGraph: {
+    type: "website",
+    locale: "sr_Latn_RS",
+    url: "https://www.rbdream.com",
+    site_name: "RB Dream",
+  },
+  facebook: {
+    appId: "1234567890",
+  },
+};
